ab82f33845718c6035235cb358ac2815257f3c89
Antonio Runcio <antonio.runcio@ecoforservice.it>
2021-12-09 16:16:46 +0100

    props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,17 +3,17 @@
 
 import './ExpenseItem.css'
 
-function ExpenseItem() {
+function ExpenseItem(props) {
 
-    const expenseDate = new Date(2021, 2 ,28);
-    const expenseTitle = 'Car insurance';
-    const expenseAmount = 294.67;
+    // const expenseDate = new Date(2021, 2 ,28);
+    // const expenseTitle = 'Car insurance';
+    // const expenseAmount = 294.67;
 
     return (
         <div className="expense-item"> 
-            <div>{expenseDate.toLocaleDateString()}</div>
-            <div className="expense-item__description"><h2>{expenseTitle}</h2></div>
-            <div className="expense-item__price">{expenseAmount}</div>
+            <div>{props.date.toLocaleString()}</div>
+            <div className="expense-item__description"><h2>{props.title}</h2></div>
+            <div className="expense-item__price">{props.amount}</div>
         </div>
 
     );
