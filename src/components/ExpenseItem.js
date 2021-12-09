96866325988b35ec6fabc46d40cb23bdd12cd6ea
Antonio Runcio <antonio.runcio@ecoforservice.it>
2021-12-09 16:27:21 +0100

    Splitting

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,13 +5,17 @@ import './ExpenseItem.css'
 
 function ExpenseItem(props) {
 
-    // const expenseDate = new Date(2021, 2 ,28);
-    // const expenseTitle = 'Car insurance';
-    // const expenseAmount = 294.67;
+    const month = props.date.toLocaleString('it-IT', { month: 'long' })
+    const day = props.date.toLocaleString('it-IT', { day: '2-digit' })
+    const year = props.date.getFullYear();
 
     return (
-        <div className="expense-item"> 
-            <div>{props.date.toLocaleString()}</div>
+        <div className="expense-item">
+            <div>
+                <div>{month}</div>
+                <div>{day}</div>
+                <div>{year}</div>
+            </div>
             <div className="expense-item__description"><h2>{props.title}</h2></div>
             <div className="expense-item__price">{props.amount}</div>
         </div>
