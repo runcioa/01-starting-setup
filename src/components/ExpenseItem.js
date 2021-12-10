c53b0ce8c4d8ad16e1f78eb820c0bbdf3932c75b
Antonio Runcio <antonio.runcio@ecoforservice.it>
2021-12-10 17:48:30 +0100

    Composing

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,6 @@
 // src\components\ExpenseItem.js
 
-
+import Card from './Card'   
 import './ExpenseItem.css'
 
 import ExpenseDate from './ExpenseDate';
@@ -9,11 +9,11 @@ function ExpenseItem(props) {
 
    
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
            <ExpenseDate date={props.date}/>
             <div className="expense-item__description"><h2>{props.title}</h2></div>
             <div className="expense-item__price">{props.amount}</div>
-        </div>
+        </Card>
 
     );
 }
