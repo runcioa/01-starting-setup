33d039ff492f25e6366bdc5e06852514d14c3f55
Antonio Runcio <antonio.runcio@ecoforservice.it>
2021-12-13 18:55:01 +0100

    primo_handlre

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,13 +6,18 @@ import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
-
+    
+    const clickHandler = () => {
+        console.log('clicked');
+    } 
    
     return (
         <Card className="expense-item">
            <ExpenseDate date={props.date}/>
-            <div className="expense-item__description"><h2>{props.title}</h2></div>
+            <div className="expense-item__description"><h2>{props.title}</h2>
             <div className="expense-item__price">{props.amount}</div>
+            </div>
+            <button onClick={clickHandler}>Change title</button>
         </Card>
 
     );
