d47049723e25063d0fd196fa726271ddcaaef63c
Antonio Runcio <antonio.runcio@ecoforservice.it>
2021-12-16 09:48:37 +0100

    Corretto_singolo

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,36 +3,22 @@ import './ExpenseForm.css'
 
 const ExpenseForm = ()=>{
 
-    // const [enteredTitle, setEnteredTitle] = useState('');
-    // const [enteredAmount, setEnteredAmount] = useState('');
-    // const [enteredDate, setEnteredDate] = useState('');
-
-
-    const [userInput, setUserInput] = useState({
-        enteredTitle: '',
-        enteredAmount: '',
-        enteredDate: ''
-
-    })
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
 
     //Il modo corretto di farlo
     const titleChangeHandler = (event) =>{
-        setUserInput((prevState)=>{
-            return {...prevState,
-            enteredTitle = event.target.value }
-        });
+        setEnteredTitle(event.target.value);
+        };
     };
     
     const amountChangeHandler = (event) =>{
-        setEnteredAmount({
-            ...userInput,
-            setEnteredAmount = event.target.value})
+        setEnteredAmount(event.target.value)
     };
 
     const dateChangeHandler = (event) => {
-        setEnteredDate({
-            ...userInput,
-            setEnteredDate = event.target.value});
+        setEnteredDate(event.target.value);
     };
 
     return(
