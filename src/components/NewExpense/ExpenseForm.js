3b2b7242952b2eb74d4668641bb4b30347477996
Antonio Runcio <antonio.runcio@ecoforservice.it>
2021-12-16 09:39:25 +0100

    multiple state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,20 +3,35 @@ import './ExpenseForm.css'
 
 const ExpenseForm = ()=>{
 
-    const [enteredTitle, setEnteredTitle] = useState('');
-    const [enteredAmount, setEnteredAmount] = useState('');
-    const [enteredDate, setEnteredDate] = useState('');
+    // const [enteredTitle, setEnteredTitle] = useState('');
+    // const [enteredAmount, setEnteredAmount] = useState('');
+    // const [enteredDate, setEnteredDate] = useState('');
+
+
+    const [userInput, setUserInput] = useState({
+        enteredTitle: '',
+        enteredAmount: '',
+        enteredDate: ''
+
+    })
 
     const titleChangeHandler = (event) =>{
-        setEnteredTitle(event.target.value)
+        setUserInput({
+            ...userInput,
+            enteredTitle = event.target.value,
+        })
     };
     
     const amountChangeHandler = (event) =>{
-        setEnteredAmount(event.target.value)
+        setEnteredAmount({
+            ...userInput,
+            setEnteredAmount = event.target.value})
     };
 
     const dateChangeHandler = (event) => {
-        setEnteredDate(event.target.value);
+        setEnteredDate({
+            ...userInput,
+            setEnteredDate = event.target.value});
     }
 
     return(
