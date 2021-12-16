cbc301278b849d0fa0dac05ab75d6db2d1405d8d
Antonio Runcio <antonio.runcio@ecoforservice.it>
2021-12-16 14:26:21 +0100

    Two_way_binding_controlledComponent_OK

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,17 +1,33 @@
+import React, {useState} from "react";
+
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css"
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
 
+    const [filteredYear, setFilterdYear]= useState('2020');
+
+    const filterChangeHandler = (selectedYear)=>{
+        setFilterdYear(selectedYear);
+       
+    };
+
+
     return (
-        <Card className="expenses">
-            <ExpenseItem
-                title={props.items[0].title}
-                amount={props.items[0].amount}
-                date={props.items[0].date}
-            />
-        </Card>
+
+        <div>
+
+            <Card className="expenses">
+                <ExpensesFilter onChangeFilter={filterChangeHandler} selected={filteredYear}/>
+                <ExpenseItem
+                    title={props.items[0].title}
+                    amount={props.items[0].amount}
+                    date={props.items[0].date}
+                />
+            </Card>
+        </div>
 
     );
 
